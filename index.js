94fa3ec8c08f5a7f9f171b30fc12467c1142167c
ankitbhardwaj78 <bhardwajankit438@gmail.com>
2017-08-14 23:41:29 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+$('.create').on('click',function(){
+    $(this).toggleClass('remvBg');
+    if($(this).hasClass('remvBg')) {
+        $('.create i').text('close');
+    }
+    else {
+        $('.create i').text('create');
+    }
+    $('#expandedBg').toggleClass('fabExp');
+    $('#options button').toggleClass('show');
+});
+
+
+jQuery(document).ready(function($) {
+
+  //Count nr. of square classes
+  var countSquare = $('.square').length;
+
+  //For each Square found add BG image
+  for (i = 0; i < countSquare; i++) {
+    var firstImage = $('.square').eq([i]);
+    var secondImage = $('.square2').eq([i]);
+
+    var getImage = firstImage.attr('data-image');
+    var getImage2 = secondImage.attr('data-image');
+
+    firstImage.css('background-image', 'url(' + getImage + ')');
+    secondImage.css('background-image', 'url(' + getImage2 + ')');
+  }
+
+});
