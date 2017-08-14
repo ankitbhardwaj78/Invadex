decb354911d3056626b9b461bfa9b64f9a0a74e7
ankitbhardwaj78 <bhardwajankit438@gmail.com>
2017-08-15 00:57:24 +0530

    minor changes

--- index.js
+++ index.js
@@ -29,3 +29,19 @@ jQuery(document).ready(function($) {
   }
 
 });
+
+
+$('.tab-content > div').hide();
+$('.tab-content > div:first-of-type').show();
+$('.tabs a').click(function(e){
+  e.preventDefault();
+    var $this = $(this),
+        tabgroup = '#'+$this.parents('.tabs').data('tabgroup'),
+        others = $this.closest('li').siblings().children('a'),
+        target = $this.attr('href');
+    others.removeClass('active');
+    $this.addClass('active');
+    $(tabgroup).children('div').hide();
+    $(target).show();
+  
+})
